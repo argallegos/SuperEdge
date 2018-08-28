a1093663e952faded3224613ee4db6d8e595174d
Alex <rag93@drexel.edu>
2018-08-28 11:07:40 -0400

    Fixes before Week 10 Build

--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -25,7 +25,7 @@ public class PlayerScript : MonoBehaviour {
     public Vector3 facingDirection;
     public Vector3 inputs = Vector3.zero;
     [HideInInspector]
-    public bool inAir, falling, sprinting = false;
+    public bool inAir, falling, jumping = false, sprinting = false;
     [HideInInspector]
     public float speed, pInputVertical, pInputHorizontal, camY;
 
@@ -74,37 +74,33 @@ public class PlayerScript : MonoBehaviour {
         inputs.z = playerInput.Vertical;
 
         if (playerInput.jump && OnGround()) Jump();
-
+        /*
         if (!OnGround())
         {
             Flying();
             inAir = true;
-        }
-        if (inAir && OnGround())
+        }*/
+        if (falling && OnGround())
         {
-            inAir = false;
+            if (inAir) inAir = false;
             falling = false;
-
+            //print("on ground!");
         }
 
         if ((playerInput.shift && !sprinting) || (!playerInput.shift && sprinting)) Sprint();
         if (!paused) Look();
-        /*
-        if (playerInput.Horizontal == 0f && playerInput.Vertical == 0f)
-        {
-            anim.SetBool("isIdle", true);
-            anim.SetBool("isRunning", false);
-            anim.SetBool("isDoubleRunning", false);
-        }*/
 
-        if (!falling && !sprinting)
+        if (playerRB.velocity.y < -0.1f) falling = true;
+
+        if (!falling && !sprinting && !inAir)
             if (inputs == Vector3.zero)
             {
                 anim.SetBool("isIdle", true);
                 anim.SetBool("isRunning", false);
                 anim.SetBool("isDoubleRunning", false);
+                anim.SetBool("isJumping", false);
             }
-
+        print("falling = " + falling + ", OnGround = " + OnGround());
     }
     private void FixedUpdate()
     {
@@ -132,6 +128,8 @@ public class PlayerScript : MonoBehaviour {
         else transform.position += transform.up * direction.x * Time.fixedDeltaTime + transform.right * direction.y * Time.fixedDeltaTime;
         anim.SetBool("isRunning", true);
         anim.SetBool("isIdle", false);
+        anim.SetBool("isJumping", false);
+
 
     }
 
@@ -139,6 +137,7 @@ public class PlayerScript : MonoBehaviour {
     {
         playerRB.AddForce(Vector3.up * jumpForce);
         inAir = true;
+        jumping = true;
         //anim.SetInteger("AnimState", 2);
         anim.SetBool("isIdle", false);
         anim.SetBool("isRunning", false);
